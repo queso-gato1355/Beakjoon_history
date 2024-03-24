a6d7eb81cc13742b9d3641abbca3c00cae5eda3b
Jung Young Chan <ingabbang@gmail.com>
2024-03-24 15:09:26 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+    cout << (1 << N) << '\n';
+
+    return 0;
+}
