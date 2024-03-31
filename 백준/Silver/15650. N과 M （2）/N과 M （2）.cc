f71539665809eae9c4cc492aeb180cd21abb9b29
Jung Young Chan <ingabbang@gmail.com>
2024-03-31 23:31:07 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+vector<int> arr;
+vector<bool> visited;
+
+void dfs(int N, int M, int idx, int before) {
+    // base case
+    if (idx == M) {
+        for(int k: arr)
+            cout << k << " ";
+        cout << '\n';
+        return;
+    }
+
+    for(int i = 0; i < N; i++) {
+        if(!visited[i] && i+1 > before) {
+            visited[i] = true;
+            arr[idx] = i+1;
+            dfs(N, M, idx + 1, i+1);
+
+            visited[i] = false;
+        }
+    }
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int M, N;
+    cin >> N >> M;
+    for(int i = 1; i <= N; i++) {
+        visited.push_back(false);
+    }
+    for(int i = 1; i <= M; i++) {
+        arr.push_back(0);
+    }
+    dfs(N, M, 0, 0);
+
+    return 0;
+}
